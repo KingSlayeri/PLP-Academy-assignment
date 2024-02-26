a4e62b6bad99f9866861185f938f176f0bbef4da
KingSlayeri <ncapailindelani3@gmail.com>
2024-02-27 00:37:12 +0200

    plp assignment one

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: constant_identifier_names, no_leading_underscores_for_local_identifiers, avoid_print, unnecessary_string_interpolations
+
+void main() {
+  const String Name = "Lindelani", Surname = "Ncapai";
+  double year = 2024;
+  
+  int age = 22, _age = 23; // _age is years i am turning.
+  
+  List<String> hobbies = ["chess", "Soccer", "Coding"];
+  Map<String, bool> mStatus = {"Married": false};//mStatus is Marriage status.
+  
+  
+
+  print("Hello My name is $Name");
+  print("");
+  print("My Surname is $Surname");
+  print("");
+  print("I am  $age years of Age," " turning $_age in the year $year");
+  print("");
+  print("My hobbies are: ");
+  
+  print("");
+  print("${hobbies[0]}");
+
+  print("${hobbies[1]}");
+
+  
+  print("${hobbies[2]}");
+  print("");
+  
+
+  print("My Marriage status is : $mStatus");
+  
+  print("YES I AM A CHRISTIAN MAN");
+  
+}
